2b7a3d981e63fa26bafae6a4e777083701905e2d
min4849 <min4849@naver.com>
2022-09-09 00:55:57 +0900

    [FE/MY]Create: 컴포넌트 파일 생성

---
+++ frontend/mgmg-project/src/views/MainPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>MainPage입니다</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ frontend/mgmg-project/src/views/MyPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>MyPage입니당</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
