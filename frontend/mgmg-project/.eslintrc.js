eb386dbd072acdf6af964e98b10ede03859ca0ae
pc5401 <pc5401@naver.com>
2022-09-10 01:04:05 +0900

    [FE/MG]Fix: CRLF LF 출동  에러 수정

---
+++ frontend/mgmg-project/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
