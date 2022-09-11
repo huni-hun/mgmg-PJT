1c2f7ad9360ace503c67a6deb31252cb4821093a
tjrcks2237 <tjrcks2237@naver.com>
2022-09-11 18:02:29 +0900

    [BE/SC]Feat: 관심 선물 카테고리 엔티티 작성

---
+++ backend/mgmg-project/src/main/java/com/ssafy/mgmgproject/db/entity/GiftCategory.java
@@ -0,0 +1,29 @@
+package com.ssafy.mgmgproject.db.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "gift_category")
+public class GiftCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gift_category_no")
+    private Long giftCategoryNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    @Column(name = "gift_category_name")
+    private String giftCategoryName;
+}
