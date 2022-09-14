c1841e2c92712f4b3e4580b85b87c5fafd81badf
min4849 <min4849@naver.com>
2022-09-14 13:10:45 +0900

    [FE/MY]Design: 회원가입페이지 그리드

--- frontend/mgmg-project/src/components/signup/SignupComponent.vue
+++ frontend/mgmg-project/src/components/signup/SignupComponent.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <label for="" id="">아이디</label>
+  <v-app id="signupComponentApp">
+    <div>회원가입</div>
+    <v-container class="signupContainer" id="signupContainer">
+      <v-col>
+        <v-row>
+          <label class="col-4" for="" id="">아이디</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="아이디를 입력하세요." />
+          <div class="col-4">
+            <custom-button btnText="중복확인" />
+          </div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">비밀번호</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="비밀번호를 입력하세요." />
+          <div class="col-4"></div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">비밀번호 확인</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="비밀번호를 다시 한 번 입력하세요." />
+          <div class="col-4"></div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">이메일</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="이메일을 입력하세요." />
+          <div class="col-4">
+            <custom-button btnText="인증하기" />
+          </div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">인증번호</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="인증번호를 입력하세요." />
+          <div class="col-4">
+            <custom-button btnText="확인" />
+          </div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">이름</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="이름을 입력하세요." />
+          <div class="col-4"></div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">생년월일</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="생일을 입력하세요." />
+          <div class="col-4"></div>
+        </v-row>
+        <v-row>
+          <label class="col-4" for="" id="">성별</label>
+          <input type="text" class="col-4" id="idSignupInput" placeholder="비밀번호를 입력하세요." />
+          <div class="col-4"></div>
+        </v-row>
+      </v-col>
+    </v-container>
+    <!-- <label for="" id="">아이디</label>
     <input type="text" id="idSignupInput" placeholder="아이디를 입력하세요." />
     <button>중복확인</button>
     <br />
@@ -26,15 +77,17 @@
     <br />
     <label for="" id="">성별</label>
     <input type="text" id="genderSignupInput" placeholder="" />
-    <br />
+    <br /> -->
     <!-- <v-calendar></v-calendar> -->
     <!-- <v-date-picker></v-date-picker> -->
     <v-date-picker v-model="date" />
-  </div>
+  </v-app>
 </template>
 
 <script>
+import CustomButton from "../common/CustomButton.vue";
 export default {
+  components: { CustomButton },
   data() {
     return {
       date: new Date(),
@@ -43,5 +96,11 @@ export default {
 };
 </script>
 
-<style></style>
-
+<style scoped>
+/* .signupContainer {
+  width: 50rem;
+} */
+/* #signupComponentApp {
+  margin: 0;
+} */
+</style>
--- frontend/mgmg-project/src/views/SignupPage.vue
+++ frontend/mgmg-project/src/views/SignupPage.vue
@@ -4,13 +4,12 @@
     <SignUp v-if="signupOrder == 1" />
     <MusicSurvey v-if="signupOrder == 2" />
     <GiftSurvey v-if="signupOrder == 3" />
-    <!-- <v-btn>dd</v-btn> -->
     <customButton btnText="이전" id="signupBeforeButton" v-if="signupOrder > 1" @click="signupBefore"></customButton>
     <customButton btnText="다음" id="signupNextButton" v-if="signupOrder < 3" @click="signupNext"></customButton>
     <customButton btnText="완료" id="signupFinish" v-if="signupOrder == 3"></customButton>
-    <button id="signupBeforeButton" v-if="signupOrder > 1" @click="signupBefore">이전</button>
+    <!-- <button id="signupBeforeButton" v-if="signupOrder > 1" @click="signupBefore">이전</button>
     <button id="signupNextButton" v-if="signupOrder < 3" @click="signupNext">다음</button>
-    <button id="signupFinish" v-if="signupOrder == 3">완료</button>
+    <button id="signupFinish" v-if="signupOrder == 3">완료</button> -->
   </div>
 </template>
 
