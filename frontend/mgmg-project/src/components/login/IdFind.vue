db33d14436b386f9ac40357759a724b3699f7975
min4849 <min4849@naver.com>
2022-09-15 21:38:36 +0900

    [FE/MY]Feat: 로그인 화면 구성

--- frontend/mgmg-project/src/components/login/IdFind.vue
+++ frontend/mgmg-project/src/components/login/IdFind.vue
@@ -1,9 +1,72 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <label for="">아이디 찾기</label>
+      <v-row>
+        <v-col>
+          <label for="">이름</label>
+        </v-col>
+        <v-col>
+          <CustomInput v-model="nameIdFindInput" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <label for="">이메일</label>
+        </v-col>
+        <v-col>
+          <CustomInput v-model="emailIdFindInput" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <CustomButton @click="findId" btnText="아이디 찾기" />
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import swal from "sweetalert2";
+export default {
+  data() {
+    return {
+      // api로 받아올 유저 아이디
+      getUserId: "",
+      // 커스텀 인풋
+      nameIdFindInput: {
+        labelText: "아이디를 입력하세요.",
+        rules: [(v) => !!v || "아이디는 필수값입니다."],
+        hint: "아이디를 입력하세요.",
+        id: "nameIdFindInput",
+      },
+      emailIdFindInput: {
+        labelText: "이메일을 입력하세요.",
+        rules: [(v) => !!v || "이메일은 필수값입니다."],
+        hint: "이메일을 입력하세요.",
+        id: "emailIdFindInput",
+      },
+    };
+  },
+  methods: {
+    test() {},
+    findId() {
+      const userName = document.getElementById("nameIdFindInput").value;
+      const userEmail = document.getElementById("emailIdFindInput").value;
+      swal.fire("Title", "Sub Title", "success");
+      axios
+        .get(this.$store.state.baseurl + "api/user/findid", {
+          userName: userName,
+          email: userEmail,
+        })
+        .then((response) => {
+          if (response.data.statusCode == 200) {
+            this.getUserId = response.data.userId;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
--- frontend/mgmg-project/src/components/login/PasswordFind.vue
+++ frontend/mgmg-project/src/components/login/PasswordFind.vue
@@ -1,9 +1,70 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <label for="">비밀번호 찾기</label>
+      <v-row>
+        <v-col>
+          <label for="">아이디</label>
+        </v-col>
+        <v-col>
+          <CustomInput v-model="idPasswordFindInput" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <label for="">이메일</label>
+        </v-col>
+        <v-col>
+          <CustomInput v-model="emailPasswordFindInput" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <CustomButton @click="findPw" btnText="비밀번호 찾기" />
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import swal from "sweetalert2";
+export default {
+  data() {
+    return {
+      // 커스텀 인풋
+      idPasswordFindInput: {
+        labelText: "아이디를 입력하세요.",
+        rules: [(v) => !!v || "아이디는 필수값입니다."],
+        hint: "아이디를 입력하세요.",
+        id: "idPasswordFindInput",
+      },
+      emailPasswordFindInput: {
+        labelText: "이메일을 입력하세요.",
+        rules: [(v) => !!v || "이메일은 필수값입니다."],
+        hint: "이메일을 입력하세요.",
+        id: "emailPasswordFindInput",
+      },
+    };
+  },
+  methods: {
+    findPw() {
+      const userId = document.getElementById("idPasswordFindInput").value;
+      const userEmail = document.getElementById("emailPasswordFindInput").value;
+
+      swal.fire("Title", "Sub Title", "success");
+      axios
+        .get(this.$store.state.baseurl + "api/user/findid", {
+          userId: userId,
+          email: userEmail,
+        })
+        .then((response) => {
+          if (response.data.statusCode == 200) {
+            console.log(response.data.message);
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
