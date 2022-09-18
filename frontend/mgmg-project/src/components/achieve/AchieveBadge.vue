05737fc15abfa6717c8396c099afcba8572bd185
pc5401 <pc5401@naver.com>
2022-09-19 00:01:29 +0900

    [FE/MG]Feat: header 디자인 수정, 업적 개발 중,

--- frontend/mgmg-project/src/components/achieve/AchieveBadge.vue
+++ frontend/mgmg-project/src/components/achieve/AchieveBadge.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    {{ badge.badgeName }}
+
+    <img v-if="badge.achievedBadgeFlag" class="badge" :src="require(`@/assets/badge/${badge.badgeName}.png`)" alt="" />
+    <img v-if="!badge.achievedBadgeFlag" class="badge" :src="require(`@/assets/badge/${badge.badgeName}Black.png`)" alt="" />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "App",
+  props: { badge: Object },
+
+  data: () => ({}),
+};
 </script>
 
-<style></style>
+<style>
+.badge {
+  height: 10rem;
+}
+</style>
