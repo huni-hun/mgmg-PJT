8765fa892382195ac762f19a185aaded8210ae05
min4849 <min4849@naver.com>
2022-09-19 11:28:50 +0900

    [FE/MY]Feat: 관심목록 페이지 화면 구성

--- frontend/mgmg-project/src/components/interestlist/InterestMusicShow.vue
+++ frontend/mgmg-project/src/components/interestlist/InterestMusicShow.vue
@@ -1,13 +1,39 @@
 <template>
   <v-container>
     <v-row>
-      <label for="">음악리스트</label>
+      <v-col>
+        <v-container v-for="music in musicLst" :key="music.title">
+          <v-row>
+            <div class="col-2">image</div>
+            <v-col class="col-8">
+              <label for="">제목: {{ music.title }}</label> <br />
+              <label for="">가수: {{ music.singer }}</label> <br />
+              <label for="">작성 일기 날짜: {{ music.date }}</label> <br />
+              <label for="">삭제아이콘</label><br />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-col>
+        <div>video</div>
+        <div>video detail</div>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      musicLst: [
+        { title: "title1", singer: "singer1", date: "2022-01-01" },
+        { title: "title2", singer: "singer2", date: "2022-02-02" },
+        { title: "title3", singer: "singer3", date: "2022-03-03" },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
