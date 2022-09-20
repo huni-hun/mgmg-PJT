57f8227dedbfda8de60fa5408d90064b3ecce9ea
pc5401 <pc5401@naver.com>
2022-09-21 01:11:34 +0900

    [FE/MG]Feat: 업적페이지 웹기능 구현

--- frontend/mgmg-project/src/components/achieve/AchieveList.vue
+++ frontend/mgmg-project/src/components/achieve/AchieveList.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <!-- 그리드랑 반복 랜더링 활용하기 -->
-    <v-container class="justify-center">
-      <v-row>
-        <v-col cols="3" v-for="badge in badges" :key="badge.badgeNo">
+    <v-container class="justify-center custom">
+      <v-btn class="left-arrow" icon @click="turnLeft">
+        <v-icon size="100">mdi-menu-left</v-icon>
+      </v-btn>
+      <!-- 일단 억지로 grid row를 5개로 줘서 했지만, 반응형이 안 됨 -->
+      <v-row class="five-cols">
+        <v-col v-for="badge in badgesList" :key="badge.badgeNo">
           <achieveBadge :badge="badge" />
         </v-col>
       </v-row>
+      <v-btn class="right-arrow" icon @click="turnRight">
+        <v-icon size="100">mdi-menu-right</v-icon>
+      </v-btn>
     </v-container>
   </div>
 </template>
@@ -17,6 +24,9 @@ export default {
   name: "NoticePage",
   data() {
     return {
+      badgeStart: 0,
+      badgeEnd: 15,
+      // Axios 통신하면서 바로 데이터를 저장하는 방식으로 구현하자.
       badges: [
         { badgeNo: 1, badgeName: "firstMeeting", achievedBadgeFlag: 0 },
         { badgeNo: 2, badgeName: "pencilBronze", achievedBadgeFlag: 1 },
@@ -56,10 +66,57 @@ export default {
         { badgeNo: 36, badgeName: "shameGold", achievedBadgeFlag: 1 },
         { badgeNo: 37, badgeName: "shameSilver", achievedBadgeFlag: 0 },
       ],
+      badgeList: [],
     };
+  },
+  methods: {
+    turnLeft() {
+      if (this.badgeStart) {
+        // 0 이 아니면,(좌측)
+        this.badgeStart -= 15;
+        this.badgeEnd -= 15;
+      }
+    },
+    turnRight() {
+      if (this.badgeEnd < 36) {
+        this.badgeStart += 15;
+        this.badgeEnd += 15;
+      }
+    },
+  },
+  created() {
+    this.badgeList = this.badges.slice(this.badgeStart, this.badgeEnd);
+  },
+  computed: {
+    badgesList() {
+      return this.badges.slice(this.badgeStart, this.badgeEnd);
+    },
   },
   components: { AchieveBadge },
 };
 </script>
 
-<style></style>
+<style scoped>
+.custom {
+  background: rgba(0, 0, 0, 0.35);
+  position: relative;
+}
+/* grid 5개로 나누는 css */
+.five-cols {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+
+.left-arrow {
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+}
+
+.right-arrow {
+  position: absolute;
+  z-index: 1;
+  top: 50%;
+  right: 0;
+}
+</style>
--- frontend/mgmg-project/src/components/achieve/NoAttainModal.vue
+++ frontend/mgmg-project/src/components/achieve/NoAttainModal.vue
@@ -1,9 +1,69 @@
 <template>
-  <div></div>
+  <div>
+    <v-img :src="require('@/assets/achieve/noattaionmodal.png')">
+      <v-card class="badge-card">
+        <img class="badge" :src="require(`@/assets/badge/${badge.badgeName}Black.png`)" alt="" />
+        <v-card-text class="badge-text">
+          <p class="text-h5 text--primary">몽글이 뱃지 이름</p>
+          <p class="text-h6 text--primary">몽글이 뱃지 내용입니다.</p>
+          <div class="card-date">
+            획득날짜<br />
+            2022.xx.xx"
+          </div>
+        </v-card-text>
+        <v-btn class="badge-btn" @click="setDialog"> 확인 </v-btn>
+      </v-card>
+    </v-img>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "AchieveBadge",
+  props: { badge: Object },
+
+  data: () => ({ dialog: false }),
+  methods: {
+    setDialog() {
+      this.$emit("set-dialog");
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.badge-card {
+  border: none;
+  background: none;
+  height: 100%;
+}
+.badge {
+  position: absolute;
+  top: 15%;
+  left: 6%;
+  height: 12rem;
+}
+
+.badge-text {
+  position: absolute;
+  top: 15%;
+  left: 40%;
+  width: 60%;
+  text-align: center;
+}
+
+.badge-btn {
+  position: absolute;
+  top: 78%;
+  left: 44%;
+  border-radius: 12px;
+}
+
+.card-date {
+  background-color: rgb(241, 244, 255);
+  width: 60%;
+  padding: 0.5rem;
+  margin: auto;
+  border-radius: 12px;
+}
+</style>
