776deb99c0eb1230caefd34b7683fcc9422d3e15
min4849 <min4849@naver.com>
2022-09-21 21:41:02 +0900

    [FE/MY]Feat: 마이페이지 비밀번호수정  api (헤더에 토큰 넣기 필요)

--- frontend/mgmg-project/src/components/mypage/PasswordEdit.vue
+++ frontend/mgmg-project/src/components/mypage/PasswordEdit.vue
@@ -47,45 +47,83 @@
       <div class="col-4"></div>
     </v-row>
     <v-row>
-      <CustomButton btnText="수정하기" />
+      <CustomButton btnText="수정하기" @click="userPasswordEdit" />
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
+import api_url from "@/api/index.js";
 export default {
   data() {
     return {
       userPw: "",
       pwValidation: false,
+      pwCheckValidation: false,
       // 비밀번호인풋
       showPw: false,
       showPwCheck: false,
       password: "Password",
       rules: {
-        pwRequired: (v) => this.pwValidationCheck(v) || "영어, 숫자, 특수문자 포함 8자 이상 16자 이하",
-        pwCheckRequired: (v) => this.pwCheckValidationCheck(v) || "비밀번호가 일치하지 않습니다.",
+        pwRequired: (v) => this.pwValidatedCheck(v) || "영어, 숫자, 특수문자 포함 8자 이상 16자 이하",
+        pwCheckRequired: (v) => this.pwCheckValidatedCheck(v) || "비밀번호가 일치하지 않습니다.",
       },
       // 비밀번호 정규식 검사
-      pwValidationCheck(user_pw) {
+      pwValidatedCheck(user_pw) {
         const regPw = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-zA-Z0-9#?!@$ %^&*-]{8,16}$/;
         if (regPw.test(user_pw)) {
           this.userPw = user_pw;
+          this.pwValidation = true;
           return true;
         } else {
           this.pwValidation = false;
         }
       },
       // 비밀번호체크 - 비밀번호 일치여부 검사
-      pwCheckValidationCheck(user_pwcheck) {
-        if (this.userPw == user_pwcheck) {
-          this.pwValidation = true;
-          return true;
-        } else {
+      pwCheckValidatedCheck(user_pwcheck) {
+        if (this.userPw != user_pwcheck) {
           this.pwCheckValidation = false;
+        } else {
+          this.pwCheckValidation = true;
+          return true;
         }
       },
     };
+  },
+  methods: {
+    userPasswordEdit() {
+      console.log(this.pwValidation, this.pwCheckValidation, this.userPw);
+      if (this.pwValidation && this.pwCheckValidation) {
+        axios
+          .put(api_url.accounts.password_edit(), {
+            headers: {
+              //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 헤더에 토큰
+              Authorization: `Bearer ${this.$store.state.userStore.accessToken}`,
+            }, //@@@@@@@@@@@@@@@@@@ 기존 pw는 왜 받지?
+            newPassword: this.userPw,
+          })
+          .then((response) => {
+            console.log(response);
+            Swal.fire({
+              text: "비밀번호가 정상적으로 변경되었습니다.",
+              icon: "success",
+              // iconColor: "#000000",
+              confirmButtonColor: "#666666",
+              confirmButtonText: "확인",
+            });
+          });
+      } else {
+        Swal.fire({
+          text: "입력한 정보를 다시 확인해주세요.",
+          icon: "warning",
+          // iconColor: "#000000",
+          confirmButtonColor: "#666666",
+          confirmButtonText: "확인",
+        });
+      }
+    },
   },
 };
 </script>
