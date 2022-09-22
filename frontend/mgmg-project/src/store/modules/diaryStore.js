4d7c601a800020dffbb94035519839b746094b9f
yeonsu-k <dustnzlzl@naver.com>
2022-09-23 00:13:13 +0900

    [FE/YS]Create: 일기 관련 Vuex 생성

---
+++ frontend/mgmg-project/src/store/modules/diaryStore.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import { diaryWrite } from "@/api/diary.js";
+
+Vue.use(Vuex);
+
+export const diaryStore = {
+  namespaced: true,
+
+  state: () => ({
+    content: "",
+    date: "",
+    img: "",
+    thema: "",
+    emotion: "",
+    gift: "",
+    music: "",
+    weather: "",
+  }),
+  mutations: {
+    setDiary(state, data) {
+      state.content = data.diaryContent;
+      state.date = data.diaryDate;
+      state.img = data.diaryImg;
+      state.thema = data.diaryThema;
+      state.emotion = data.emotion;
+      state.gift = data.gift;
+      state.music = data.music;
+      state.weather = data.weather;
+    },
+  },
+  actions: {
+    async fetchDiary({ commit }, diaryData) {
+      await diaryWrite(diaryData)
+        .then((res) => {
+          console.log("vuex success", res.data);
+          commit("setDiary", res.data);
+        })
+        .catch((error) => console.log("vuex error", error));
+    },
+  },
+  modules: {},
+};
+
+export default diaryStore;
