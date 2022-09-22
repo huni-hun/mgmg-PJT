c6a2cbb93a8672970b9b956c714bf0c5ee365c00
t21212 <t21212@naver.com>
2022-09-22 12:59:02 +0900

    [BE/TH]Modify: front 배포용 vue.config 일부 수정

--- frontend/mgmg-project/vue.config.js
+++ frontend/mgmg-project/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   devServer: {
+    host: "0.0.0.0",
+    port: 8080,
+    open: true,
     allowedHosts: "all",
     client: {
       webSocketURL: "wss://j7b306.p.ssafy.io:443/ws",
