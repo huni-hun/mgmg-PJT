cb8cf704e841cc48b672d687b1a004cb2f29adc1
min4849 <min4849@naver.com>
2022-09-22 10:23:59 +0900

    [FE/MY]Feat: 회원 탈퇴

--- frontend/mgmg-project/src/components/mypage/UserDelete.vue
+++ frontend/mgmg-project/src/components/mypage/UserDelete.vue
@@ -18,7 +18,40 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import api_url from "@/api/index.js";
+import Swal from "sweetalert2";
+export default {
+  userDelete() {
+    axios
+      .delete(api_url.accounts.mypage_show_edit_delete(), {
+        headers: {
+          //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 헤더에 토큰
+          Authorization: `Bearer ${this.$store.state.userStore.accessToken}`,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        Swal.fire({
+          text: "정상적으로 탈퇴 처리 되었습니다.",
+          icon: "success",
+          // iconColor: "#000000",
+          confirmButtonColor: "#666666",
+          confirmButtonText: "확인",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        Swal.fire({
+          text: "데이터 전송에 실패했습니다.",
+          icon: "warning",
+          // iconColor: "#000000",
+          confirmButtonColor: "#666666",
+          confirmButtonText: "확인",
+        });
+      });
+  },
+};
 </script>
 
 <style scoped></style>
