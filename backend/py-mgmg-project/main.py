22a7e1d2a0749f78fefdd6498ecdec0cd4d7b22e
yunaghgh <yunaghgh@naver.com>
2022-09-26 01:06:42 +0900

    [BE/YN]Feat: 음악 추천 api 작성

---
+++ backend/py-mgmg-project/main.py
@@ -0,0 +1,19 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+from recommendation import getMusicRecommendations
+
+
+app = FastAPI()
+
+class RecommendMusicRequest(BaseModel):
+    user_id: str
+    diary_result: List[float]=[]
+    
+
+@app.post('/recommendation/music')
+async def recommendMusic(recommendMusicRequest: RecommendMusicRequest):
+    result = getMusicRecommendations(recommendMusicRequest)
+    return result
+    
+    
