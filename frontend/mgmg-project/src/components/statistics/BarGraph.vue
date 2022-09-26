8239965cd547dda256ffc98ac6d5e3586374c86f
pc5401 <pc5401@naver.com>
2022-09-26 10:08:57 +0900

    [FE/MG]Feat: 통계 페이지 제작

--- frontend/mgmg-project/src/components/statistics/BarGraph.vue
+++ frontend/mgmg-project/src/components/statistics/BarGraph.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <Bar
+    <Bar
       :chart-options="chartOptions"
       :chart-data="chartData"
       :chart-id="chartId"
@@ -10,22 +10,22 @@
       :styles="styles"
       :width="width"
       :height="height"
-    /> -->
+    />
     <h3>{{ statistics }}</h3>
   </div>
 </template>
 
 <script>
-// import { Bar } from "vue-chartjs";
+import { Bar } from "vue-chartjs";
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
+import { statistics_day } from "@/store/modules/etcStore";
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
   name: "BarChart",
-  // components: { Bar },
+  components: { Bar },
   props: {
-    statistics: {},
     chartId: {
       type: String,
       default: "bar-chart",
@@ -57,66 +57,82 @@ export default {
   },
   data() {
     return {
+      dayData: Object,
+      statistics: Object,
+
       chartData: {
         labels: ["월", "화", "수", "목", "금", "토", "일"],
-        // datasets: [{ data: [40, 20, 12] }],
-        datasets: [
-          {
-            label: "보통",
-            data: null,
-            backgroundColor: "rgb(255, 99, 132)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "기쁨",
-            data: null,
-            backgroundColor: "rgb(255, 199, 132)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "사랑",
-            data: null,
-            backgroundColor: "rgb(155, 99, 232)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "부끄",
-            data: null,
-            backgroundColor: "rgb(205, 99, 232)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "기대",
-            data: null,
-            backgroundColor: "rgb(155, 199, 132)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "슬픔",
-            data: null,
-            backgroundColor: "rgb(2, 99, 132)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "짜증",
-            data: null,
-            backgroundColor: "rgb(25, 199, 132)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "피곤",
-            data: null,
-            backgroundColor: "rgb(25, 99, 12)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "화남",
-            data: null,
-            backgroundColor: "rgb(55, 99, 12)", // 색상 직접 찾아서 입력
-          },
-          {
-            label: "공포",
-            data: null,
-            backgroundColor: "rgb(255, 99, 132)", // 색상 직접 찾아서 입력
-          },
-        ],
+        datasets: [],
       },
       chartOptions: {
         responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true,
+          },
+        },
       },
     };
+  },
+  async created() {
+    this.dayData = await statistics_day();
+    this.statistics = this.dayData.statistics;
+
+    this.chartData.datasets = [
+      {
+        label: "슬픔",
+        data: this.statistics.슬픔,
+        backgroundColor: "rgb(118, 127, 227)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "공포",
+        data: this.statistics.공포,
+        backgroundColor: "rgb(110, 98, 146)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "피곤",
+        data: this.statistics.피곤,
+        backgroundColor: "rgb(177, 181, 184)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "화",
+        data: this.statistics.화,
+        backgroundColor: "rgb(238, 101, 99)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "기대",
+        data: this.statistics.기대,
+        backgroundColor: "rgb(225, 245, 254)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "평온",
+        data: this.statistics.평온,
+        backgroundColor: "rgb(255, 255, 255)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "창피",
+        data: this.statistics.창피,
+        backgroundColor: "rgb(249, 181, 118)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "짜증",
+        data: this.statistics.짜증,
+        backgroundColor: "rgb(215, 95, 166)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "기쁨",
+        data: this.statistics.기쁨,
+        backgroundColor: "rgb(255, 231, 154)", // 색상 직접 찾아서 입력
+      },
+      {
+        label: "사랑",
+        data: this.statistics.사랑,
+        backgroundColor: "rgb(248, 181, 175)", // 색상 직접 찾아서 입력
+      },
+    ];
   },
 };
 </script>
--- frontend/mgmg-project/src/store/modules/etcStore.js
+++ frontend/mgmg-project/src/store/modules/etcStore.js
@@ -39,4 +39,71 @@ async function statistics_day() {
   return response.data;
 }
 
-export { achieve_list, achieve_detail, achieve_get_list, statistics_day };
+async function statistics_percent(params) {
+  const response = await API.get(`/statistics/percent`, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+    params: {
+      startDate: params.startDate,
+      endDate: params.endDate,
+    },
+  });
+  return response.data;
+}
+
+async function notice_get(params) {
+  // 공지사항 조회
+  const response = await API.get(`/notice`, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+    params: {
+      page: params.page,
+      keyword: params.keyword,
+    },
+  });
+  return response.data;
+}
+
+async function notice_post(request) {
+  // 공지사항 작성
+  const response = await API.get(`/notice`, request, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+  });
+  return response.data;
+}
+
+async function notice_detail_get(pid) {
+  // 공지사항 페이지 읽기
+  const response = await API.get(`/notice/${pid}`, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+  });
+  return response.data;
+}
+
+async function notice_detail_put(pid, request) {
+  // 공지사항 수정
+  const response = await API.get(`/notice/${pid}`, request, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+  });
+  return response.data;
+}
+
+async function notice_detail_delete(pid) {
+  // 공지사항 삭제
+  const response = await API.get(`/notice/${pid}`, {
+    headers: {
+      Authorization: `Bearer ${store.state.accessToken}`,
+    },
+  });
+  return response.data;
+}
+
+export { achieve_list, achieve_detail, achieve_get_list, statistics_day, statistics_percent, notice_get, notice_post, notice_detail_get, notice_detail_put, notice_detail_delete };
