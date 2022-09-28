b93c602e2f761e0c627e387a1b42208a3d765f70
yeonsu-k <dustnzlzl@naver.com>
2022-09-28 16:54:40 +0900

    [FE/YS[Feat: 일기 관련 API 추가

--- frontend/mgmg-project/src/api/diary.js
+++ frontend/mgmg-project/src/api/diary.js
@@ -107,6 +107,69 @@ async function giftOpen(pid) {
   return response.data;
 }
 
+async function detailMusic(pid) {
+  let response = await API.get(`${DIARY}/detail/${pid}/music`, {
+    headers: {
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
+async function detailGift(pid) {
+  let response = await API.get(`${DIARY}/detail/${pid}/gift`, {
+    headers: {
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
+async function cancleGift(no) {
+  let response = await API.delete(`interest/gift`, {
+    data: { giftNo: no },
+    headers: {
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
+async function cancleGoodMusic(no) {
+  let response = await API.delete(`interest/music`, {
+    data: { musicNo: no },
+    headers: {
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
+async function cancleBadMusic(pid) {
+  let response = await API.delete(`${DIARY}/badmusic/${pid}`, {
+    headers: {
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
+async function sttWrite(formData) {
+  let response = await API.post(`${DIARY}/speeach`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: "Bearer " + store.state.accessToken,
+    },
+  });
+
+  return response.data;
+}
+
 export {
   diaryWrite,
   monthlyDiaryList,
@@ -117,4 +180,10 @@ export {
   musicBad,
   giftInterest,
   giftOpen,
+  detailMusic,
+  detailGift,
+  cancleGift,
+  cancleGoodMusic,
+  cancleBadMusic,
+  sttWrite,
 };
