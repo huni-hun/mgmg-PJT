1ecdf516a7467278909735df0d106f67a6c1b9c2
yeonsu-k <dustnzlzl@naver.com>
2022-09-29 16:05:52 +0900

    [FE/YS]Feat: 모든 페이지 틀 반응형 적용

--- frontend/mgmg-project/src/App.vue
+++ frontend/mgmg-project/src/App.vue
@@ -2,7 +2,10 @@
   <v-app>
     <CustomHeader ref="header" v-if="(`landing`!=$route.name)" />
     <v-main>
-      <router-view />
+      <router-view v-if="(`landing`==$route.name)" />
+      <v-container v-else class="app_view justify-center">
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
