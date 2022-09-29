ce8445040d7dc605dd784df9905f828e6fd3a99d
min4849 <min4849@naver.com>
2022-09-29 09:23:51 +0900

    [FE/MY]Feat: 마이페이지 관심음악장르 변경 수정된 화면과 api 변경 완료

--- frontend/mgmg-project/src/components/mypage/MusicEdit.vue
+++ frontend/mgmg-project/src/components/mypage/MusicEdit.vue
@@ -1,45 +1,54 @@
 <template>
   <div>
-    <v-container>
+    <v-container v-if="interestMusicPage == 1">
       <v-row>음악 장르 (1/2)</v-row>
       <v-row>감정별로 선호하는 음악 장르를 선택하세요.</v-row>
+      <!-- <v-row>
+        <v-checkbox label="happy" :value="happy" input-value="checkBox" @change="test()" v-model="happy"></v-checkbox>
+      </v-row> -->
       <v-row><hr class="hrStyle" /></v-row>
       <v-row>
-        <v-col
-          class="genreCheckLst"
-          v-for="(emotion, index) in emotionLst1"
-          :key="index"
-        >
+        <v-col class="genreCheckLst" v-for="(emotion, index) in emotionLst1" :key="index">
           <v-container>
             <v-row>
-              <img
-                :src="
-                  require(`@/assets/emoticon/${emotionEnglishLst1[index]}.png`)
-                "
-                alt=""
-                class="emoticonImg"
-              />
+              <img :src="require(`@/assets/emoticon/${emotionEnglishLst1[index]}.png`)" alt="" class="emoticonImg" />
             </v-row>
             <v-row>
               <div>{{ emotion }}</div>
             </v-row>
             <v-row>
               <v-col>
-                <v-checkbox
-                  hide-details
-                  class="genreCheckBox"
-                  v-for="(genre, index) in genreLst1"
-                  :key="index"
-                  :label="genreLst1[index]"
-                  :value="genreLst1[index]"
-                  @click="addGenres(emotion, genreLst1[index])"
-                ></v-checkbox>
+                <v-checkbox hide-details class="genreCheckBox" v-for="(genre, idx) in genreLst" :key="idx" :label="genreLst[idx]" :value="genre" v-model="input1[emotion][idx]"></v-checkbox>
               </v-col>
             </v-row>
           </v-container>
         </v-col>
       </v-row>
       <v-row><CustomButton btnText="다음" @click="musicEditNext1Page" /></v-row>
+    </v-container>
+    <v-container v-if="interestMusicPage == 2">
+      <v-row>음악 장르 (2/2)</v-row>
+      <v-row>감정별로 선호하는 음악 장르를 선택하세요.</v-row>
+      <v-row><hr class="hrStyle" /></v-row>
+      <v-row>
+        <v-col class="genreCheckLst" v-for="(emotion, index) in emotionLst2" :key="index">
+          <v-container>
+            <v-row>
+              <img :src="require(`@/assets/emoticon/${emotionEnglishLst2[index]}.png`)" alt="" class="emoticonImg" />
+            </v-row>
+            <v-row>
+              <div>{{ emotion }}</div>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-checkbox hide-details class="genreCheckBox" v-for="(genre, idx) in genreLst" :key="idx" :label="genreLst[idx]" :value="genre" v-model="input1[emotion][idx]"></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+      <v-row><CustomButton btnText="이전" @click="musicEditBefore1Page" /></v-row>
+      <v-row><CustomButton btnText="완료" @click="musicEditFinish" /></v-row>
     </v-container>
   </div>
 </template>
@@ -52,21 +61,29 @@ import { changeInterestMusic } from "@/api/userApi.js";
 export default {
   data() {
     return {
+      happy: "",
       interestMusicPage: 1,
 
       emotionLst1: ["평온", "기쁨", "사랑", "짜증", "피곤"],
       emotionEnglishLst1: ["calm", "happy", "love", "annoyed", "fatigue"],
+      emotionLst2: ["기대", "슬픔", "창피", "화", "공포"],
+      emotionEnglishLst2: ["expect", "sad", "shame", "angry", "fear"],
+      emotionLst: ["평온", "기쁨", "사랑", "짜증", "피곤", "기대", "슬픔", "창피", "화", "공포"],
 
-      genreLst1: [
-        "R&B/Soul",
-        "댄스",
-        "랩/힙합",
-        "록/메탈",
-        "발라드",
-        "인디음악",
-        "트로트",
-        "포크/블루스",
-      ],
+      genreLst: ["R&B/Soul", "댄스", "랩/힙합", "록/메탈", "발라드", "인디음악", "트로트", "포크/블루스"],
+      input1: {
+        평온: ["", "", "", "", "", "", "", ""],
+        기쁨: ["", "", "", "", "", "", "", ""],
+        사랑: ["", "", "", "", "", "", "", ""],
+        짜증: ["", "", "", "", "", "", "", ""],
+        피곤: ["", "", "", "", "", "", "", ""],
+        기대: ["", "", "", "", "", "", "", ""],
+        슬픔: ["", "", "", "", "", "", "", ""],
+        창피: ["", "", "", "", "", "", "", ""],
+        화: ["", "", "", "", "", "", "", ""],
+        공포: ["", "", "", "", "", "", "", ""],
+      },
+      testInput: true,
 
       musicTaste: {
         피곤: [],
@@ -86,26 +103,37 @@ export default {
   //   this.setgenreLst2();
   // },
   methods: {
+    test() {
+      console.log(this.happy);
+    },
     // setgenreLst2() {
-    //   this.genreLst2 = [...this.genreLst1];
+    //   this.genreLst2 = [...this.genreLst];
     // },
     musicEditBefore1Page() {
       this.interestMusicPage--;
+      console.log("이전페이지 넘어감", this.input1);
     },
     musicEditNext1Page() {
-      console.log(this.musicTaste);
+      console.log("다음버튼누름", this.input1);
       var isAllChecked = true;
       var rep;
+      var gen;
+      var isNotEmpty = false;
       //하나라도 비어있는 항목 있으면 안됨.
       for (rep = 0; rep < 5; rep++) {
-        console.log(this.emotionLst1[rep]);
-        console.log(this.musicTaste[this.emotionLst1[rep]]);
-        if (
-          typeof this.musicTaste[this.emotionLst1[rep]] == "undefined" ||
-          this.musicTaste[this.emotionLst1[rep]].length == 0
-        ) {
-          isAllChecked = false;
+        for (gen = 0; gen < 8; gen++) {
+          if (this.input1[this.emotionLst1[rep]][gen]) {
+            isNotEmpty = true;
+            // console.log("감정은", this.emotionLst1[rep]);
+            // console.log(this.input1[this.emotionLst1[rep]][gen]);
+          }
         }
+        console.log("isNotEmpty", isNotEmpty);
+        if (isNotEmpty == false) {
+          isAllChecked = false;
+          // console.log("isAllChecked", isAllChecked);
+        }
+        isNotEmpty = false;
       }
       if (!isAllChecked) {
         Swal.fire({
@@ -117,21 +145,30 @@ export default {
         });
       } else {
         this.interestMusicPage++;
+        console.log("다음페이지 넘어감", this.input1);
       }
     },
     musicEditFinish() {
+      console.log("최종단계시작", this.input1);
       var isAllChecked = true;
       var rep;
+      var gen;
+      var isNotEmpty = false;
       //하나라도 비어있는 항목 있으면 안됨.
       for (rep = 0; rep < 5; rep++) {
-        console.log(this.emotionLst2[rep]);
-        console.log(this.musicTaste[this.emotionLst2[rep]]);
-        if (
-          typeof this.musicTaste[this.emotionLst2[rep]] == "undefined" ||
-          this.musicTaste[this.emotionLst2[rep]].length == 0
-        ) {
-          isAllChecked = false;
+        for (gen = 0; gen < 8; gen++) {
+          if (this.input1[this.emotionLst2[rep]][gen]) {
+            isNotEmpty = true;
+            console.log("감정은", this.emotionLst2[rep]);
+            console.log(this.input1[this.emotionLst2[rep]][gen]);
+          }
         }
+        console.log("isNotEmpty", isNotEmpty);
+        if (isNotEmpty == false) {
+          isAllChecked = false;
+          console.log("isAllChecked", isAllChecked);
+        }
+        isNotEmpty = false;
       }
       if (!isAllChecked) {
         Swal.fire({
@@ -142,42 +179,22 @@ export default {
           confirmButtonText: "확인",
         });
       } else {
+        var emoRep;
+        for (emoRep = 0; emoRep < 10; emoRep++) {
+          var genRep;
+          for (genRep = 0; genRep < 8; genRep++) {
+            console.log(this.musicTaste[this.emotionLst[emoRep]]);
+            if (this.input1[this.emotionLst[emoRep]][genRep] != "") {
+              this.musicTaste[this.emotionLst[emoRep]].push(this.input1[this.emotionLst[emoRep]][genRep]);
+            }
+            console.log(this.musicTaste[this.emotionLst[emoRep]]);
+          }
+        }
         this.mypageMusicEdit();
+        console.log(this.musicTaste);
       }
     },
 
-    addGenres(emotion, genre) {
-      // 선택 리스트의 감정에 해당 장르가 없으면 추가
-      if (!this.musicTaste[emotion].includes(genre)) {
-        this.musicTaste[emotion].push(genre);
-      } else {
-        //있으면 삭제
-        this.musicTaste[emotion].pop(genre);
-      }
-      console.log(this.musicTaste);
-    },
-
-    // // 음악 선택. 선택리스트에 없으면 추가, 있으면 제거
-    // addMusic(music) {
-    //   if (this.selectedMusic.includes(music)) {
-    //     // if (this.selectedMusic.length == 1) {
-    //     //   return;
-    //     // }
-    //     this.selectedMusic.splice(this.selectedMusic.indexOf(music), 1);
-    //   } else if (this.selectedMusic.length >= 5) {
-    //     return;
-    //   } else {
-    //     this.selectedMusic.push(music);
-    //   }
-    // },
-    //음악 리스트 조회
-    // async showInterestMusic() {
-    //   let response = await showInterestMusic();
-    //   console.log("응답 데이터", response);
-    //   if (response.statusCode == 200) {
-    //     this.selectedMusic = response.musicGenres;
-    //   }
-    // },
     // 음악 리스트 변경
     async mypageMusicEdit() {
       var request = {
@@ -194,38 +211,10 @@ export default {
           confirmButtonColor: "#666666",
           confirmButtonText: "확인",
         });
+
+        this.$router.push("/my/myinfo");
       }
     },
-    // mypageMusicEdit() {
-    //   axios
-    //     .put(api_url.accounts.interest_music_edit(), {
-    //       headers: {
-    //         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 헤더에 토큰
-    //         Authorization: `Bearer ${this.$store.state.userStore.accessToken}`,
-    //       },
-    //       musicTaste: this.selectedMusic,
-    //     })
-    //     .then((response) => {
-    //       console.log(response);
-    //       Swal.fire({
-    //         text: "음악 장르가 정상적으로 변경되었습니다.",
-    //         icon: "success",
-    //         // iconColor: "#000000",
-    //         confirmButtonColor: "#666666",
-    //         confirmButtonText: "확인",
-    //       });
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //       Swal.fire({
-    //         text: "음악 장르 변경에 실패했습니다.",
-    //         icon: "warning",
-    //         // iconColor: "#000000",
-    //         confirmButtonColor: "#666666",
-    //         confirmButtonText: "확인",
-    //       });
-    //     });
-    // },
   },
   components: { CustomButton },
 };
@@ -244,8 +233,7 @@ export default {
   box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25);
 }
 .selected {
-  box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25),
-    inset 3px 3px 4px 3px rgba(0, 0, 0, 0.38);
+  box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25), inset 3px 3px 4px 3px rgba(0, 0, 0, 0.38);
 }
 .genreCheckLst {
   width: 20%;
