7746affc33a144a547387068e845764c61bbae2d
yunaghgh <yunaghgh@naver.com>
2022-09-30 10:20:49 +0900

    [FE/YN]Modify: 취향 선물 조회시 최저가, 최고가 데이터 불러오기

--- frontend/mgmg-project/src/components/mypage/GiftEdit.vue
+++ frontend/mgmg-project/src/components/mypage/GiftEdit.vue
@@ -1,17 +1,38 @@
 <template>
   <v-container>
     <v-row @click="test">관심 선물</v-row>
-    <v-row>좋아하는 선물 종류를 선택하세요. 최소 1개, 최대 5개까지 선택할 수 있습니다.</v-row>
+    <v-row
+      >좋아하는 선물 종류를 선택하세요. 최소 1개, 최대 5개까지 선택할 수
+      있습니다.</v-row
+    >
     <v-row><hr class="hrStyle" /></v-row>
     <v-row>
-      <div class="flex" v-for="gift in giftLst.slice(0, 5)" :key="gift" @click="addGift(gift)">
-        <div id="circle" class="giftLstBox" :class="{ selected: selectedGift.includes(gift) }"></div>
+      <div
+        class="flex"
+        v-for="gift in giftLst.slice(0, 5)"
+        :key="gift"
+        @click="addGift(gift)"
+      >
+        <div
+          id="circle"
+          class="giftLstBox"
+          :class="{ selected: selectedGift.includes(gift) }"
+        ></div>
         <div>{{ gift }}</div>
       </div>
     </v-row>
     <v-row>
-      <div class="flex" v-for="gift in giftLst.slice(5, 10)" :key="gift" @click="addGift(gift)">
-        <div id="circle" class="giftLstBox" :class="{ selected: selectedGift.includes(gift) }"></div>
+      <div
+        class="flex"
+        v-for="gift in giftLst.slice(5, 10)"
+        :key="gift"
+        @click="addGift(gift)"
+      >
+        <div
+          id="circle"
+          class="giftLstBox"
+          :class="{ selected: selectedGift.includes(gift) }"
+        ></div>
         <div>{{ gift }}</div>
       </div>
     </v-row>
@@ -27,9 +48,27 @@
       </v-col>
       <v-col>
         <div>
-          <input type="number" id="underPrice" name="underPrice" :value="underPrice" step="1000" min="10000" :max="upperPrice - 1000" @change="changeUnder()" />
+          <input
+            type="number"
+            id="underPrice"
+            name="underPrice"
+            :value="underPrice"
+            step="1000"
+            min="10000"
+            :max="upperPrice - 1000"
+            @change="changeUnder()"
+          />
           <label for="">~</label>
-          <input type="number" id="upperPrice" name="upperPrice" :value="upperPrice" step="1000" :min="underPrice" max="1000000" @change="changeUpper()" />
+          <input
+            type="number"
+            id="upperPrice"
+            name="upperPrice"
+            :value="upperPrice"
+            step="1000"
+            :min="underPrice"
+            max="1000000"
+            @change="changeUpper()"
+          />
         </div>
       </v-col>
     </v-row>
@@ -49,7 +88,18 @@ export default {
       underPrice: 10000,
       upperPrice: 20000,
 
-      giftLst: ["패션의류", "패션잡화", "화장품/미용", "디지털/가전", "가구/인테리어", "출산/육아", "식품", "스포츠/레저", "생활/건강", "여가/생활편의"],
+      giftLst: [
+        "패션의류",
+        "패션잡화",
+        "화장품/미용",
+        "디지털/가전",
+        "가구/인테리어",
+        "출산/육아",
+        "식품",
+        "스포츠/레저",
+        "생활/건강",
+        "여가/생활편의",
+      ],
       selectedGift: ["패션의류"],
       // this.$store.state.userStore.interestGiftLstSignup,
 
@@ -114,13 +164,11 @@ export default {
     },
     // 선물 리스트 조회
     async showaInterestGift() {
-      let response = await showInterestGift();
-      console.log("응답 데이터", response);
-      if (response.statusCode == 200) {
-        this.selectedGift = response.giftCategories;
-        // this.underPrice = response.lowPrice;
-        // this.lupperPrice = response.highPrice;
-      }
+      await showInterestGift().then((res) => {
+        this.selectedGift = res.giftCategories;
+        this.underPrice = res.lowPrice;
+        this.upperPrice = res.highPrice;
+      });
     },
     // 선물 리스트 변경
     async mypageGiftEdit() {
@@ -130,51 +178,27 @@ export default {
         highPrice: this.upperPrice,
       };
 
-      let response = await changeInterestGift(request);
-      console.log("응답 데이터", response);
-      if (response.statusCode == 200) {
-        Swal.fire({
-          text: "선물 종류가 정상적으로 변경되었습니다.",
-          icon: "success",
-          // iconColor: "#000000",
-          confirmButtonColor: "#666666",
-          confirmButtonText: "확인",
+      await changeInterestGift(request)
+        .then((res) => {
+          console.log(res);
+          Swal.fire({
+            text: "선물 종류가 정상적으로 변경되었습니다.",
+            icon: "success",
+            // iconColor: "#000000",
+            confirmButtonColor: "#666666",
+            confirmButtonText: "확인",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          Swal.fire({
+            text: "잠시후 다시 시도해주세요.",
+            icon: "warning",
+            confirmButtonColor: "#666666",
+            confirmButtonText: "확인",
+          });
         });
-      }
     },
-
-    // mypageGiftEdit() {
-    //   axios
-    //     .put(api_url.accounts.interest_gift_edit(), {
-    //       headers: {
-    //         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 헤더에 토큰
-    //         Authorization: `Bearer ${this.$store.state.userStore.accessToken}`,
-    //       },
-    //       lowPrice: document.getElementById("priceUnder").value,
-    //       highPrice: document.getElementById("priceUpper").value,
-    //       giftTaste: this.selectedGift,
-    //     })
-    //     .then((response) => {
-    //       console.log(response);
-    //       Swal.fire({
-    //         text: "관심 선물이 정상적으로 변경되었습니다.",
-    //         icon: "success",
-    //         // iconColor: "#000000",
-    //         confirmButtonColor: "#666666",
-    //         confirmButtonText: "확인",
-    //       });
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //       Swal.fire({
-    //         text: "관심 선물 변경에 실패했습니다.",
-    //         icon: "warning",
-    //         // iconColor: "#000000",
-    //         confirmButtonColor: "#666666",
-    //         confirmButtonText: "확인",
-    //       });
-    //     });
-    // },
   },
 };
 </script>
@@ -192,7 +216,8 @@ export default {
   box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25);
 }
 .selected {
-  box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25), inset 3px 3px 4px 3px rgba(0, 0, 0, 0.38);
+  box-shadow: 0px 0px 4px 5px rgba(99, 99, 99, 0.25),
+    inset 3px 3px 4px 3px rgba(0, 0, 0, 0.38);
 }
 #circle {
   width: 100px;
