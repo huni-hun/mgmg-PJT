ec49d1df9566d3e9d13796777a8922113aabee56
min4849 <min4849@naver.com>
2022-10-03 00:12:33 +0900

    [FE/MY]Design: 마이페이지 회원탈퇴 css

--- frontend/mgmg-project/src/components/mypage/UserDelete.vue
+++ frontend/mgmg-project/src/components/mypage/UserDelete.vue
@@ -1,21 +1,24 @@
 <template>
-  <div>
-    <v-container>
-      <v-row>
+  <div class="userDeleteTotalBody">
+    <div class="userDeleteTitle">
+      <div class="titleLabel">
         <label for="">회원 탈퇴</label>
-      </v-row>
-      <v-row>
+      </div>
+      <!-- <div>
+        <img class="titleImg" src="../../assets/emoticon/sad.png" alt="" />
+      </div> -->
+    </div>
+    <div class="userDeleteBody">
+      <div class="userDeleteWarning">
         <label for="">※ 회원 탈퇴 시 모든 개인 정보는 삭제됩니다.</label>
-      </v-row>
-      <v-row>
-        <label for=""
-          >※ 탈퇴 처리 후에는 아이디 및 데이터를 복구할 수 없습니다.</label
-        >
-      </v-row>
-      <v-row>
-        <CustomButton btnText="탈퇴하기" @click="userDelete" />
-      </v-row>
-    </v-container>
+      </div>
+      <div>
+        <label for="">※ 탈퇴 처리 후에는 아이디 및 데이터를 복구할 수 없습니다.</label>
+      </div>
+    </div>
+    <div class="userDeleteButtonLine">
+      <CustomButton class="userDeleteButton" btnText="탈퇴하기" @click="userDelete" />
+    </div>
   </div>
 </template>
 
@@ -89,4 +92,49 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.userDeleteTotalBody {
+  width: 100%;
+  padding: 7% 10%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+}
+.userDeleteTitle {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: left;
+  align-items: center;
+}
+.titleLabel {
+  font-size: clamp(1.2rem, 2.5vw, 1.8rem);
+  margin: 3% 0% 0.5% 0%;
+  /* width: 70%; */
+  /* display: block; */
+}
+.titleImg {
+  width: 5%;
+}
+
+.userDeleteBody {
+  margin-top: 5%;
+}
+.userDeleteWarning {
+  margin-bottom: 2%;
+}
+
+.userDeleteButtonLine {
+  width: 100%;
+  margin-top: 10%;
+  display: flex;
+  justify-content: center;
+}
+.userDeleteButton {
+  width: 50%;
+  background-color: #fa5f72;
+}
+.userDeleteButton:hover {
+  background-color: #ac424f;
+}
+</style>
