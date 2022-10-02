c3856054f44c800ab1ce4c2971486535a3c754d4
min4849 <min4849@naver.com>
2022-10-02 18:00:31 +0900

    [FE/MY]Design: 마이페이지 배너, 전체적 구조

--- frontend/mgmg-project/src/views/MyPage.vue
+++ frontend/mgmg-project/src/views/MyPage.vue
@@ -1,90 +1,61 @@
 <template>
-  <v-container>
-    <h1>Mypage입니다.</h1>
-    <v-row>
-      <v-col class="col-3">
-        <v-container>
+  <v-container class="mypageTotalBody">
+    <div class="mypageBody">
+      <div class="mypageBanner">
+        <div class="bannerTitleLabel">
           <label for="">마이페이지</label>
-          <!-- <hr class="hrStyle" />
-          <router-link to="/my/mycheck">
-            <div><label for="">본인 인증</label></div>
-          </router-link> -->
-          <hr class="hrStyle" />
-          <router-link to="/my/myinfo">
-            <div><label for="">내 정보 확인</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/infoedit">
-            <div><label for="">회원 정보 변경</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/passwordedit">
-            <div><label for="">비밀번호 수정</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/musicedit">
-            <div><label for="">음악 장르 변경</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/giftedit">
-            <div><label for="">선물 종류 변경</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/fontedit">
-            <div><label for="">글꼴 변경</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <router-link to="/my/userdelete">
-            <div><label for="">회원 탈퇴</label></div>
-          </router-link>
-          <hr class="hrStyle" />
-          <!-- <div v-for="menu in menuLst" :key="menu.id" @click="menuOrderChange(menu.id)">
-            <label for="" class="menuLstBox" :class="{ nowMenu: menu.id == menuOrder }">{{ menu.menuName }}</label>
-            <hr class="hrStyle" />
-          </div> -->
-          <!-- <label for="">회원 정보 수정</label>
-          <hr class="hrStyle" />
-          <label for="">비밀번호 수정</label>
-          <hr class="hrStyle" />
-          <label for="">음악 장르 변경</label>
-          <hr class="hrStyle" />
-          <label for="">선물 종류 변경</label>
-          <hr class="hrStyle" />
-          <label for="">글꼴 변경</label>
-          <hr class="hrStyle" />
-          <label for="">회원 탈퇴</label>
-          <hr class="hrStyle" /> -->
-        </v-container>
-      </v-col>
-      <v-col class="col-9">
+        </div>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/myinfo">
+          <div class="bannerMenuLabel"><label for="">내 정보 확인</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/infoedit">
+          <div class="bannerMenuLabel"><label for="">회원 정보 변경</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/passwordedit">
+          <div class="bannerMenuLabel"><label for="">비밀번호 수정</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/musicedit">
+          <div class="bannerMenuLabel"><label for="">음악 장르 변경</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/giftedit">
+          <div class="bannerMenuLabel"><label for="">선물 종류 변경</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/fontedit">
+          <div class="bannerMenuLabel"><label for="">글꼴 변경</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+
+        <router-link class="bannerLabelDecoration" to="/my/userdelete">
+          <div class="bannerMenuLabel"><label for="">회원 탈퇴</label></div>
+        </router-link>
+        <hr class="hrStyle" />
+      </div>
+
+      <div class="mypageView">
         <router-view></router-view>
-        <!-- <MyCheck v-if="menuOrder == 'MyCheck'" />
-        <MyInfo v-if="menuOrder == 'MyInfo'" />
-        <InfoEdit v-if="menuOrder == 'InfoEdit'" />
-        <PasswordEdit v-if="menuOrder == 'PasswordEdit'" />
-        <MusicEdit v-if="menuOrder == 'MusicEdit'" />
-        <GiftEdit v-if="menuOrder == 'GiftEdit'" />
-        <FontEdit v-if="menuOrder == 'FontEdit'" />
-        <UserDelete v-if="menuOrder == 'UserDelete'" /> -->
-      </v-col>
-    </v-row>
+      </div>
+    </div>
   </v-container>
 </template>
 
 <script>
-// import MyCheck from "@/components/mypage/MyCheck.vue";
-// import MyInfo from "@/components/mypage/MyInfo.vue";
-// import InfoEdit from "@/components/mypage/InfoEdit.vue";
-// import PasswordEdit from "@/components/mypage/PasswordEdit.vue";
-// import MusicEdit from "@/components/mypage/MusicEdit.vue";
-// import GiftEdit from "@/components/mypage/GiftEdit.vue";
-// import FontEdit from "@/components/mypage/FontEdit.vue";
-// import UserDelete from "@/components/mypage/UserDelete.vue";
 export default {
   components: {},
   data() {
     return {
-      menuOrder: "MyCheck",
+      menuOrder: 0,
       menuLst: [
         { id: "MyInfo", menuName: "내 정보 확인" },
         { id: "PasswordEdit", menuName: "비밀번호 수정" },
@@ -105,6 +76,39 @@ export default {
 </script>
 
 <style scoped>
+.mypageTotalBody {
+  border: 2px solid black;
+  margin-top: 5vw;
+  /* height: 50vh; */
+}
+.mypageBody {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+.mypageBanner {
+  width: 20%;
+  padding: 1% 1% 1% 1%;
+}
+.bannerTitleLabel {
+  font-size: clamp(1.2rem, 2.5vw, 1.8rem);
+  margin: 5% 0;
+  padding-left: 5%;
+}
+.bannerMenuLabel {
+  margin: 3% 0;
+  padding-left: 5%;
+}
+.bannerLabelDecoration {
+  color: black;
+  text-decoration: none;
+}
+
+.mypageView {
+  width: 80%;
+  padding: 1%;
+}
+
 .hrStyle {
   border: 0.01rem solid #000000;
   width: 100%;
