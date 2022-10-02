ba217b9f4cbb5e1b822fd1639463b64b1cbe2ce8
min4849 <min4849@naver.com>
2022-10-02 23:41:07 +0900

    [FE/MY]Design: 마이페이지 비밀번호변경 css

--- frontend/mgmg-project/src/components/mypage/PasswordEdit.vue
+++ frontend/mgmg-project/src/components/mypage/PasswordEdit.vue
@@ -1,65 +1,72 @@
 <template>
-  <v-container>
-    <v-row>
+  <div class="passwordEditTotalBody">
+    <div class="titleLabel">
       <label for="">비밀번호 수정</label>
-    </v-row>
-    <v-row>
+    </div>
+
+    <div>
       <hr class="hrStyle" />
-    </v-row>
-    <v-row>
-      <label
-        class="col-4 PasswordEditNoDrag"
-        for="pwPasswordEditInput"
-        id="pwPasswordEditLabel"
-        >새로운 비밀번호</label
-      >
-      <div class="inputStyle col-4">
-        <v-text-field
-          :append-icon="showPw ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.pwRequired]"
-          :type="showPw ? 'text' : 'password'"
-          label="비밀번호를 입력하세요."
-          name="input-10-2"
-          hint="비밀번호를 입력하세요."
-          value=""
-          class="input-group--focused"
-          @click:append="showPw = !showPw"
-          single-line
-          outlined
-          id="pwPasswordEditInput"
-        ></v-text-field>
+    </div>
+
+    <div class="passwordEditBody">
+      <div class="passwordEditLine">
+        <div class="passwordEditBodyLabel">
+          <label class="PasswordEditNoDrag" for="pwPasswordEditInput" id="pwPasswordEditLabel">새 비밀번호</label>
+        </div>
+
+        <div class="passwordEditBodyContent">
+          <div class="inputStyle">
+            <v-text-field
+              :append-icon="showPw ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.pwRequired]"
+              :type="showPw ? 'text' : 'password'"
+              label="비밀번호를 입력하세요."
+              name="input-10-2"
+              hint="비밀번호를 입력하세요."
+              value=""
+              class="input-group--focused"
+              @click:append="showPw = !showPw"
+              single-line
+              outlined
+              id="pwPasswordEditInput"
+            ></v-text-field>
+          </div>
+        </div>
       </div>
-      <div class="col-4"></div>
-    </v-row>
-    <v-row>
-      <label
-        class="col-4 PasswordEditNoDrag"
-        for="pwCheckPasswordEditInput"
-        id="pwCheckPasswordEditLabel"
-        >비밀번호 확인</label
-      >
-      <div class="inputStyle col-4">
-        <v-text-field
-          :append-icon="showPwCheck ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.pwCheckRequired]"
-          :type="showPwCheck ? 'text' : 'password'"
-          label="비밀번호를 다시 입력하세요."
-          name="input-10-2"
-          hint="비밀번호를 다시 입력하세요."
-          value=""
-          class="input-group--focused"
-          @click:append="showPwCheck = !showPwCheck"
-          single-line
-          outlined
-          id="pwCheckPasswordEditInput"
-        ></v-text-field>
+      <!-- <div class=""></div> -->
+      <!-- </div>
+
+      <div class="passwordEditBody"> -->
+      <div class="passwordEditLine">
+        <div class="passwordEditBodyLabel">
+          <label class="PasswordEditNoDrag" for="pwCheckPasswordEditInput" id="pwCheckPasswordEditLabel">비밀번호 확인</label>
+        </div>
+
+        <div class="passwordEditBodyContent">
+          <div class="inputStyle">
+            <v-text-field
+              :append-icon="showPwCheck ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.pwCheckRequired]"
+              :type="showPwCheck ? 'text' : 'password'"
+              label="비밀번호를 다시 입력하세요."
+              name="input-10-2"
+              hint="비밀번호를 다시 입력하세요."
+              value=""
+              class="input-group--focused"
+              @click:append="showPwCheck = !showPwCheck"
+              single-line
+              outlined
+              id="pwCheckPasswordEditInput"
+            ></v-text-field>
+          </div>
+        </div>
       </div>
-      <div class="col-4"></div>
-    </v-row>
-    <v-row>
-      <CustomButton btnText="수정하기" @click="userPasswordEdit" />
-    </v-row>
-  </v-container>
+    </div>
+
+    <div class="passwordEditButtonLine">
+      <CustomButton class="passwordEditButton" btnText="수정하기" @click="userPasswordEdit" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -78,16 +85,12 @@ export default {
       showPwCheck: false,
       password: "Password",
       rules: {
-        pwRequired: (v) =>
-          this.pwValidatedCheck(v) ||
-          "영어, 숫자, 특수문자 포함 8자 이상 16자 이하",
-        pwCheckRequired: (v) =>
-          this.pwCheckValidatedCheck(v) || "비밀번호가 일치하지 않습니다.",
+        pwRequired: (v) => this.pwValidatedCheck(v) || "영어, 숫자, 특수문자 포함 8자 이상 16자 이하",
+        pwCheckRequired: (v) => this.pwCheckValidatedCheck(v) || "비밀번호가 일치하지 않습니다.",
       },
       // 비밀번호 정규식 검사
       pwValidatedCheck(user_pw) {
-        const regPw =
-          /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-zA-Z0-9#?!@$ %^&*-]{8,16}$/;
+        const regPw = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-])[a-zA-Z0-9#?!@$ %^&*-]{8,16}$/;
         if (regPw.test(user_pw)) {
           this.userPw = user_pw;
           this.pwValidation = true;
@@ -178,8 +181,58 @@ export default {
 </script>
 
 <style scoped>
+.passwordEditTotalBody {
+  width: 100%;
+  padding: 7% 10%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+}
+
+.titleLabel {
+  font-size: clamp(1.2rem, 2.5vw, 1.8rem);
+  margin: 3% 0% 0.5% 0%;
+}
+
+.passwordEditBody {
+  padding: 5% 3% 0% 2%;
+}
+.passwordEditLine {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2%;
+}
+.passwordEditBodyLabel {
+  width: 25%;
+}
+.passwordEditBodyContent {
+  width: 75%;
+}
+.passwordEditButtonLine {
+  width: 100%;
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+}
+.passwordEditButton {
+  width: 50%;
+}
+.passwordEditGenderContent {
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.genderButton {
+  width: 49%;
+}
+.inputStyle:deep(fieldset) {
+  /* border-color: rgb(255, 250, 250); */
+  box-shadow: 1px 1px 10px 1px rgb(209, 213, 221);
+  border-radius: 0px;
+  padding: 0;
+}
 .hrStyle {
   border: 0.01rem solid #000000;
-  width: 80rem;
 }
 </style>
