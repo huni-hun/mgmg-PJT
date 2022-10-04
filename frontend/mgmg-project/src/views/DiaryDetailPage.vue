ce8ffa069879ff3be767366e1c833761c6f34b4f
yeonsu-k <dustnzlzl@naver.com>
2022-10-05 07:11:58 +0900

    [FE/YS]Feat: 일기상세에서 달력, 관심목록으로 이동 버튼 추가

--- frontend/mgmg-project/src/views/DiaryDetailPage.vue
+++ frontend/mgmg-project/src/views/DiaryDetailPage.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="back">
     <div class="inner">
-    <DiaryDetail/>
-    <MusicRecommend/>
-    <GiftRecommend/>
-  </div>
+      <DiaryDetail />
+      <MusicRecommend />
+      <GiftRecommend />
+      <div class="detail-bottom">
+        <custom-button class="button" btnText="달력으로 가기" @click="getMain()" />
+        <custom-button class="button" btnText="관심 목록 조회" @click="interestlist()" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +16,32 @@
 import DiaryDetail from '@/components/diarydetail/DiaryDetail.vue';
 import GiftRecommend from '@/components/diarydetail/GiftRecommend.vue';
 import MusicRecommend from '@/components/diarydetail/MusicRecommend.vue';
-export default { components: { DiaryDetail, GiftRecommend, MusicRecommend } };
+import CustomButton from '@/components/common/CustomButton.vue';
+export default {
+  components: { DiaryDetail, GiftRecommend, MusicRecommend, CustomButton },
+  methods: {
+    getMain() {
+      this.$router.push("/main");
+    },
+    interestlist() {
+      this.$router.push("/interestlist");
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.detail-bottom {
+  width: 100%;
+  margin-top: 40px;
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+}
+
+.button {
+  border-radius: 30px;
+  margin: 0px 20px
+}
+</style>
