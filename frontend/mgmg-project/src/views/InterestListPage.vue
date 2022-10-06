32dd732ad0162a3537c8db9f1928bae0cfe45828
min4849 <min4849@naver.com>
2022-10-06 10:57:09 +0900

    [FE/MY]Design: 관심 목록 리스트 아이콘 변경

--- frontend/mgmg-project/src/views/InterestListPage.vue
+++ frontend/mgmg-project/src/views/InterestListPage.vue
@@ -5,8 +5,8 @@
         <label for="">관심 음악 리스트</label>
       </div>
       <div>
-        <router-link to="/interestlist/music">
-          <CustomButton btnText="선물" @click="interestOrderChange" />
+        <router-link class="iconCircle" to="/interestlist/music">
+          <v-icon @click="interestOrderChange" color="#ffffff" size="clamp(1.2rem, 3vw, 2rem)">mdi-gift-outline</v-icon>
         </router-link>
       </div>
     </div>
@@ -16,9 +16,8 @@
         <label for="">관심 선물 리스트</label>
       </div>
       <div>
-        <router-link to="/interestlist/gift">
-          <!-- <v-icon @click="interestOrderChange">mdi-music-circle</v-icon> -->
-          <CustomButton btnText="음악" @click="interestOrderChange" />
+        <router-link class="iconCircle" to="/interestlist/gift">
+          <v-icon @click="interestOrderChange" color="#ffffff" size="clamp(1.2rem, 3vw, 2rem)">mdi-music</v-icon>
         </router-link>
       </div>
     </div>
@@ -60,5 +59,19 @@ export default {
   font-size: clamp(1.5rem, 5vw, 2.2rem);
   color: white;
   /* margin-bottom: 2%; */
+}
+.iconCircle {
+  display: flex;
+  padding: 10px !important;
+  border-radius: 50%;
+  padding: 2%;
+  color: white;
+  text-decoration: none;
+
+  /* border: 2px solid white; */
+  box-shadow: 0px 0px 5px 5px rgba(47, 47, 47, 0.163);
+
+  /* background-color: #51516e; */
+  background-color: #3949ab;
 }
 </style>
